13ca878a782a6872090645a3b933b0a76095947c
Rased002 <72910525+Rased002@users.noreply.github.com>
2021-02-12 12:12:39 +0300

    ui update
    added system call to clear.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 int main(void){
     short size; // n where game square is n x n
     short player_turn = 0;
-
+    
+    system("clear");
     std::vector<std::vector<char> > position(size,std::vector<char>(size, ' '));
     std::cout << "\n\n\t\t\t\t****** Welcome to the game of Tic Tac Toe ******\n\n\n\n\n"<< std::endl;
     std::cout << "Enter game size : ";
@@ -17,13 +18,15 @@ int main(void){
     while(1){
         
         display_game(position, size);
-        take_input(position, player_turn, size);   
+        take_input(position, player_turn, size);
+        system("clear");
+        std::cout << "\n\n\n\n\t\t\t\t****** Welcome to the game of Tic Tac Toe ******"<< std::endl;
 
         if(char winner = game_status(position, size)){
             display_game(position, size);
             std::cout <<"\n\t\t\t\t\t\t****** "<< winner << " is The Winner ****** \n\n\n\n\n" << std::endl;
             break;
-        }else{std::cout<< "\n\n\n\n\n\n\n\n\n\n\n";}
+        }else{std::cout<< "\n\n\n\n\n\n";}
 
         player_turn++;
         // Max play count is size*size
